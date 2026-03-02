3e813a7b3f0cf0bf0fbce1e04043f5d8ca529e5a
barackwilliam <barackwilliam12@gmail.com>
2026-03-03 01:41:17 +0300

    changed dashboard button

--- config/settings.py
+++ config/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-67v28s=*bf@a$^9)of-_3d)99_*h_=7tx0ij&8h@2o!y!y&i7w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
 
-# Application definition
-
+# Application definition arakaza@123
+#bizimana@123
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
